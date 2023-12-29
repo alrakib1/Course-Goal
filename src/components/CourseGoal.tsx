75fea4568ee0fbe86c525a7f047ac7e5049a07fd
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2023-12-29 14:19:06 +0600

    first commit

---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,15 @@
+
+
+const CourseGoal = (props: ) => {
+    return (
+        <article>
+            <div>
+                <h2>Title</h2>
+                <p>Description</p>
+            </div>
+            <button>Delete</button>
+        </article>
+    );
+};
+
+export default CourseGoal;
