3c57457f3d14425afbf8ab4171158677e9b2a99d
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2023-12-31 11:37:42 +0600

    added mongoose

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res) => {
   res.send("Course Goal Server!");
@@ -10,3 +16,33 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`course goal server running on : http://localhost:${port}`);
 });
+
+
+
+
+const goalSchema = new mongoose.Schema({
+    goal: {
+        type: String,
+        required: [true, "Goal is required"],
+        trim: true
+    },
+    summary:{
+        type: String,
+        required: [true, "summary is required"],
+        trim: true
+    }
+})
+
+
+const GoalMode = mongoose.model("Goals",goalSchema);
+
+
+const connectDB = async ()=>{
+    try {
+        await mongoose.connect(process.env.url);
+        console.log('Mongodb is connected');
+    } catch (error) {
+        console.log(error.message);
+        process.exit(1);
+    }
+}
