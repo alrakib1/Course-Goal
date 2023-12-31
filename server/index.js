c38078993113c27a1e85f67ee6983cbff9e631c5
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2023-12-31 11:54:03 +0600

    added post method for single goal

--- server/index.js
+++ server/index.js
@@ -8,41 +8,54 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const goalSchema = new mongoose.Schema({
+  goal: {
+    type: String,
+    required: [true, "Goal is required"],
+    trim: true,
+  },
+  summary: {
+    type: String,
+    required: [true, "summary is required"],
+    trim: true,
+  },
+});
+
+const Goals = mongoose.model("Goals", goalSchema);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.url);
+    console.log("Mongodb is connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+
+app.listen(port, async () => {
+  console.log(`course goal server running on : http://localhost:${port}`);
+  await connectDB();
+});
 
 app.get("/", (req, res) => {
   res.send("Course Goal Server!");
 });
 
-app.listen(port, () => {
-  console.log(`course goal server running on : http://localhost:${port}`);
+app.post("/all", async (req, res) => {
+  try {
+    const newGoal = new Goals({
+      goal: req.body.goal,
+      summary: req.body.summary,
+    });
+
+    const newGoalData = await newGoal.save();
+
+    res
+      .status(201)
+      .send({ message: "goal has been added", success: true, result: newGoalData });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
 
-
-
-
-const goalSchema = new mongoose.Schema({
-    goal: {
-        type: String,
-        required: [true, "Goal is required"],
-        trim: true
-    },
-    summary:{
-        type: String,
-        required: [true, "summary is required"],
-        trim: true
-    }
-})
-
-
-const GoalMode = mongoose.model("Goals",goalSchema);
-
-
-const connectDB = async ()=>{
-    try {
-        await mongoose.connect(process.env.url);
-        console.log('Mongodb is connected');
-    } catch (error) {
-        console.log(error.message);
-        process.exit(1);
-    }
-}
